8b32b9df1787e44dbcf8fdb5216afcdc913da979
Matthew Elwin <elwin@northwestern.edu>
2025-05-13 10:16:33 -0500

    Adding some core ROS packages

--- channel/ros-noetic-core.scm
+++ channel/ros-noetic-core.scm
@@ -36,8 +36,125 @@
               (lambda* (#:key inputs #:allow-other-keys)
                       (substitute* "cmake/templates/python_distutils_install.sh.in"
                         (("/usr/bin/env") (search-input-file inputs "/bin/env"))))))))
-    (native-inputs (list python python-catkin-pkg python-empy))
+    (native-inputs (list python))
+    (propagated-inputs (list python-catkin-pkg python-empy))
     (home-page "http://wiki.ros.org/catkin")
     (synopsis "catkin build tool")
     (description "ROS 1 Catkin build tool")
     (license license:bsd-3))))
+
+(define-public ros-noetic-genmsg
+  (let ((commit "393871225e1458d2a8db41761759e57ca01a1801")
+        (revision "0"))
+    (package
+      (name "ros-noetic-genmsg")
+      (version (git-version "0.6.1" revision commit))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference (url "https://github.com/ros/genmsg")
+                           (commit commit)))
+       (sha256
+        (base32 "06z6fvkfifkjv58fkr9m0hfcjn279l056agclqgy4xmsvg3f8p0j"))
+       (file-name (git-file-name name version))))
+    (build-system cmake-build-system)
+    (native-inputs (list catkin python))
+     (home-page "https://docs.ros.org/en/api/genmsg/html/")
+     (synopsis "Decouple code generation from .msg .srv files from build system")
+     (description "Project genmsg exists in order to decouple code generation from .msg & .srv format
+files from the parsing of these files and from impementation details of the build system
+(project directory layout, existence or nonexistence of utilities like rospack, values of environment
+variables such as ROS_PACKAGE_PATH): i.e. none of these are required to be set in any particular way.")
+     (license license:bsd-3))))
+
+(define-public ros-noetic-gencpp
+  (let ((commit "cc7e11cf67ec5c5f49b1d539e80475073f3864b4")
+        (revision "0"))
+    (package
+      (name "ros-noetic-gencpp")
+      (version (git-version "0.7.2" revision commit))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference (url "https://github.com/ros/gencpp")
+                           (commit commit)))
+       (sha256
+        (base32 "1nwia7j7390x8574cw1iz4w1phv8q67w3khykfphsssbbxly3v44"))
+       (file-name (git-file-name name version))))
+    (build-system cmake-build-system)
+    (native-inputs (list catkin python ros-noetic-genmsg))
+     (home-page "https://docs.ros.org/en/api/genmsg/html/")
+     (synopsis "Decouple code generation from .msg .srv files from build system")
+     (description "Project genmsg exists in order to decouple code generation from .msg & .srv format
+files from the parsing of these files and from impementation details of the build system
+(project directory layout, existence or nonexistence of utilities like rospack, values of environment
+variables such as ROS_PACKAGE_PATH): i.e. none of these are required to be set in any particular way.")
+     (license license:bsd-3))))
+
+
+;~~  - gencpp
+;~~  - geneus
+;~~  - genlisp
+;~~  - gennodejs
+;~~  - genpy
+;~~  - cmake_modules
+;~~  - class_loader
+;~~  - common_msgs
+;~~  - cpp_common
+;~~  - message_generation
+;~~  - message_runtime
+;~~  - mk
+;~~  - ros
+;~~  - ros_comm
+;~~  - ros_core
+;~~  - ros_environment
+;~~  - rosbag_migration_rule
+;~~  - rosbash
+;~~  - rosboost_cfg
+;~~  - rosbuild
+;~~  - rosclean
+;~~  - roscpp_core
+;~~  - roscpp_traits
+;~~  - roscreate
+;~~  - rosgraph
+;~~  - roslang
+;~~  - roslisp
+;~~  - rosmake
+;~~  - rosmaster
+;~~  - rospack
+;~~  - roslib
+;~~  - rosparam
+;~~  - rospy
+;~~  - rosservice
+;~~  - rostime
+;~~  - roscpp_serialization
+;~~  - roslaunch
+;~~  - rosunit
+;~~  - rosconsole
+;~~  - pluginlib
+;~~  - rosconsole_bridge
+;~~  - roslz4
+;~~  - rostest
+;~~  - std_msgs
+;~~  - actionlib_msgs
+;~~  - diagnostic_msgs
+;~~  - geometry_msgs
+;~~  - nav_msgs
+;~~  - rosgraph_msgs
+;~~  - shape_msgs
+;~~  - std_srvs
+;~~  - trajectory_msgs
+;~~  - visualization_msgs
+;~~  - xmlrpcpp
+;~~  - roscpp
+;~~  - rosout
+;~~  - message_filters
+;~~  - rosbag_storage
+;~~  - rosmsg
+;~~  - rosnode
+;~~  - rostopic
+;~~  - topic_tools
+;~~  - rosbag
+;~~  - roswtf
+;~~  - sensor_msgs
+;~~  - stereo_msgs
