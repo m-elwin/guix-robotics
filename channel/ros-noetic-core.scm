2aac900a09a7bc8f75235c0a4e89d47879e0286e
Matthew Elwin <elwin@northwestern.edu>
2025-05-16 21:34:24 -0500

    Added geneus genlisp and gennodejs
    I don't really care about any of these nodes since I don't use these
    languages. They are here, but because they are part of the core rosinstall,
    but they are not tested.

--- channel/ros-noetic-core.scm
+++ channel/ros-noetic-core.scm
@@ -111,69 +111,69 @@ variables such as ROS_PACKAGE_PATH): i.e. none of these are required to be set i
      (synopsis "ROS C++ message definition and serialization generators")
      (description "Generate ROS msgs and srvs for C++")
      (license license:bsd-3))))
-;
-;(define-public ros-noetic-geneus
-;  (let ((commit "ec388e279ce4fd52ec78a4144ba52014ab4dd824")
-;        (revision "0"))
-;    (package
-;      (name "ros-noetic-geneus")
-;      (version (git-version "3.0.0" revision commit))
-;    (source
-;     (origin
-;       (method git-fetch)
-;       (uri (git-reference (url "https://github.com/jsk-ros-pkg/geneus")
-;                           (commit commit)))
-;       (sha256
-;        (base32 "0hz6jy7f9wxn9m1ni4zawmiqk87zqc0hcf652prxnlm79xri0aj6"))
-;       (file-name (git-file-name name version))))
-;    (build-system cmake-build-system)
-;    (native-inputs (list catkin python ros-noetic-genmsg))
-;     (home-page "https://github.com/jsk-ros-pkg/geneus")
-;     (synopsis "EusLisp ROS message and service generators")
-;     (description "EusLisp ROS message and service generators.")
-;     (license license:bsd-3))))
-;
-;(define-public ros-noetic-genlisp
-;  (let ((commit "3ac633abacdf5ab321d23ed013c7d5b7da97736d")
-;        (revision "0"))
-;    (package
-;      (name "ros-noetic-genlisp")
-;      (version (git-version "0.4.18" revision commit))
-;    (source
-;     (origin
-;       (method git-fetch)
-;       (uri (git-reference (url "https://github.com/ros/genlisp")
-;                           (commit commit)))
-;       (sha256
-;        (base32 "07qx0blkfx6n8i5c5mj1yzpi9y099npw4f0rzbs5p3gjpq2gh0m2"))
-;       (file-name (git-file-name name version))))
-;    (build-system cmake-build-system)
-;    (native-inputs (list catkin python ros-noetic-genmsg))
-;     (home-page "https://github.com/ros/genlisp")
-;     (synopsis "Common-Lisp ROS message and service generators")
-;     (description "Common-Lisp ROS emssage and service generators.")
-;     (license license:bsd-3))))
-;
-;(define-public ros-noetic-gennodejs
-;  (let ((commit "53cf97ce44a6f592c69cd6f6d07bb7825b6ea243")
-;        (revision "0"))
-;    (package
-;      (name "ros-noetic-gennodejs")
-;      (version (git-version "2.0.2" revision commit))
-;    (source
-;     (origin
-;       (method git-fetch)
-;       (uri (git-reference (url "https://github.com/sloretz/gennodejs")
-;                           (commit commit)))
-;       (sha256
-;        (base32 "0ybiih22mmcbkdcpyv6b4yfnssmc5n8ksl74ghvkjpyr55h7k6v7"))
-;       (file-name (git-file-name name version))))
-;    (build-system cmake-build-system)
-;    (native-inputs (list catkin python ros-noetic-genmsg))
-;     (home-page "https://github.com/ros/genlisp")
-;     (synopsis "Javascript ROS message and service generators")
-;     (description "Javascript ROS emssage and service generators.")
-;     (license license:bsd-3))))
+
+(define-public ros-noetic-geneus
+  (let ((commit "ec388e279ce4fd52ec78a4144ba52014ab4dd824")
+        (revision "0"))
+    (package
+      (name "ros-noetic-geneus")
+      (version (git-version "3.0.0" revision commit))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference (url "https://github.com/jsk-ros-pkg/geneus")
+                           (commit commit)))
+       (sha256
+        (base32 "0hz6jy7f9wxn9m1ni4zawmiqk87zqc0hcf652prxnlm79xri0aj6"))
+       (file-name (git-file-name name version))))
+    (build-system catkin-build-system)
+    (native-inputs (list ros-noetic-genmsg))
+     (home-page "https://github.com/jsk-ros-pkg/geneus")
+     (synopsis "EusLisp ROS message and service generators")
+     (description "EusLisp ROS message and service generators.")
+     (license license:bsd-3))))
+
+(define-public ros-noetic-genlisp
+  (let ((commit "3ac633abacdf5ab321d23ed013c7d5b7da97736d")
+        (revision "0"))
+    (package
+      (name "ros-noetic-genlisp")
+      (version (git-version "0.4.18" revision commit))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference (url "https://github.com/ros/genlisp")
+                           (commit commit)))
+       (sha256
+        (base32 "07qx0blkfx6n8i5c5mj1yzpi9y099npw4f0rzbs5p3gjpq2gh0m2"))
+       (file-name (git-file-name name version))))
+    (build-system catkin-build-system)
+    (native-inputs (list ros-noetic-genmsg))
+     (home-page "https://github.com/ros/genlisp")
+     (synopsis "Common-Lisp ROS message and service generators")
+     (description "Common-Lisp ROS emssage and service generators.")
+     (license license:bsd-3))))
+
+(define-public ros-noetic-gennodejs
+  (let ((commit "53cf97ce44a6f592c69cd6f6d07bb7825b6ea243")
+        (revision "0"))
+    (package
+      (name "ros-noetic-gennodejs")
+      (version (git-version "2.0.2" revision commit))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference (url "https://github.com/sloretz/gennodejs")
+                           (commit commit)))
+       (sha256
+        (base32 "0ybiih22mmcbkdcpyv6b4yfnssmc5n8ksl74ghvkjpyr55h7k6v7"))
+       (file-name (git-file-name name version))))
+    (build-system catkin-build-system)
+    (native-inputs (list ros-noetic-genmsg))
+     (home-page "https://github.com/ros/genlisp")
+     (synopsis "Javascript ROS message and service generators")
+     (description "Javascript ROS emssage and service generators.")
+     (license license:bsd-3))))
 ;
 ;(define-public ros-noetic-genpy
 ;  (let ((commit "323d861c512ba912a8c7e842647fe2d9657fd15b")
