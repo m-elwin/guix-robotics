6b3e03971383ec2e3beacca6892dc7247153d193
Matthew Elwin <elwin@northwestern.edu>
2025-05-19 07:56:47 -0500

    Added rostime

--- channel/ros-noetic-core.scm
+++ channel/ros-noetic-core.scm
@@ -261,6 +261,109 @@ variables such as ROS_PACKAGE_PATH): i.e. none of these are required to be set i
       (description "Message packages depend on this metapackage to automatically bring in all message generators.")
       (license license:bsd-3))))
 
+
+(define-public ros-noetic-cpp-common
+  (let ((commit "2951f0579a94955f5529d7f24bb1c8c7f0256451")
+        (revision "0"))
+    (package
+      (name "ros-noetic-cpp-common")
+      (version (git-version "0.7.3" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/ros/roscpp_core")
+                             (commit commit)))
+         (sha256
+          (base32 "0zs0wlkldjkvyi2d74ri93hykbq2a5wmkb1x0jibnashlyiijiwj"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (native-inputs (list boost console-bridge))
+      (arguments (list
+                  #:phases #~(modify-phases %standard-phases
+                               ;; go to the directory for the ros package
+                     (add-after 'unpack 'switch-to-cpp-core
+                       (lambda _ (chdir "cpp_common"))))))
+      (home-page "https://github.com/ros/roscpp_core")
+      (synopsis "C++ code for doing things that are not necessarily ROS related")
+      (description "C++ code for doing things that are not necessarily ROS
+related, but are useful for multiple packages. This includes things like
+the ROS_DEPRECATED and ROS_FORCE_INLINE macros, as well as code for getting
+backtraces.")
+      (license license:bsd-3))))
+
+(define-public ros-noetic-rostime
+  (let ((commit "2951f0579a94955f5529d7f24bb1c8c7f0256451")
+        (revision "0"))
+    (package
+      (name "ros-noetic-rostime")
+      (version (git-version "0.7.3" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/ros/roscpp_core")
+                             (commit commit)))
+         (sha256
+          (base32 "0zs0wlkldjkvyi2d74ri93hykbq2a5wmkb1x0jibnashlyiijiwj"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (native-inputs (list boost googletest ros-noetic-cpp-common))
+      (arguments (list
+                  #:phases #~(modify-phases %standard-phases
+                               ;; go to the directory for the ros package
+                     (add-after 'unpack 'switch-to-rostime
+                       (lambda _ (chdir "rostime"))))))
+      (home-page "http://wiki.ros.org/roscpp/Overview/Time")
+      (synopsis "Time and Duration implementation for C++ libraries, including roscpp")
+      (description "Time and Duration implementation for C++ libraries, including roscpp.")
+      (license license:bsd-3))))
+
+;(define-public ros-noetic-roscpp-serialization
+;  (let ((commit "2951f0579a94955f5529d7f24bb1c8c7f0256451")
+;        (revision "0"))
+;    (package
+;      (name "ros-noetic-roscpp-serialization")
+;      (version (git-version "0.7.3" revision commit))
+;      (source
+;       (origin
+;         (method git-fetch)
+;         (uri (git-reference (url "https://github.com/ros/roscpp_core")
+;                             (commit commit)))
+;         (sha256
+;          (base32 "0zs0wlkldjkvyi2d74ri93hykbq2a5wmkb1x0jibnashlyiijiwj"))
+;         (file-name (git-file-name name version))))
+;      (build-system catkin-build-system)
+;      (native-inputs (list boost console-bridge))
+;      (arguments (list
+;                  #:phases #~(modify-phases %standard-phases
+;                               ;; go to the directory for the ros package
+;                     (add-after 'unpack 'switch-to-roscpp-serialization
+;                       (lambda _ (chdir "roscpp_serialization"))))))
+;      (home-page "https://github.com/ros/roscpp_core")
+;      (synopsis "Serialization, as described at https://www.ros.org/wiki/roscpp/Overview/MessageSerializationAndAdaptingTypes")
+;      (description "Enables serializaing/deserializing ROS messages to memory")
+;      (license license:bsd-3))))
+
+(define-public ros-noetic-message-runtime
+  (let ((commit "6dd393ba9c6a398784da4039c162fc9186f19796")
+        (revision "0"))
+    (package
+      (name "ros-noetic-message-runtime")
+      (version (git-version "0.4.13" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/ros/message_runtime")
+                             (commit commit)))
+         (sha256
+          (base32 "1m36aknbv42m4jjaacnclm4frk5hg6aw9nql26jiphcfk3559iir"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (inputs (list ros-noetic-cpp-common ros-noetic-roscpp-serialization ros-noetic-roscpp-traits ros-noetic-rostime ros-noetic-genpy))
+      (home-page "https://github.com/ros/message_runtime")
+      (synopsis "Meta Package for the runtime components of ROS messages")
+      (description "Message packages depend on this metapackage at runtime to automatically bring in all message generators.")
+      (license license:bsd-3))))
+
 ;;~~  - common_msgs
 ;;~~  - cpp_common
 ;;~~  - message_runtime
