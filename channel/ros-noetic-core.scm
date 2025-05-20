98816c88641170e7eb5b4919e6a2c673ffb52935
Matthew Elwin <elwin@northwestern.edu>
2025-05-20 13:38:23 -0500

    Added trajectory messages.

--- channel/ros-noetic-core.scm
+++ channel/ros-noetic-core.scm
@@ -673,6 +673,38 @@ primitives (cube, sphere, etc.), planes, and meshes.")
       (description "Messages specific to stereo processing, such as disparity images.")
       (license license:bsd-3))))
 
+(define-public ros-noetic-trajectory-msgs
+  (let ((commit "1230f39a7068d1d73d1039eb0eb970c922b6bcf7")
+        (revision "0"))
+    (package
+      (name "ros-noetic-trajectory-msgs")
+      (version (git-version "1.13.2" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/ros/common_msgs")
+                             (commit commit)))
+         (sha256
+          (base32 "02wqhg70a2h3fsfkavcpvk5rvfy1nai2094irvpywmc0w4wd46sm"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (native-inputs (list ros-noetic-message-generation))
+      (propagated-inputs (list
+               ros-noetic-message-runtime
+               ros-noetic-geometry-msgs
+               ros-noetic-std-msgs
+               ros-noetic-rosbag-migration-rule))
+      (arguments (list
+                  #:phases #~(modify-phases %standard-phases
+                               ;; go to the directory for the ros package
+                     (add-after 'unpack 'switch-to-pkg-src
+                       (lambda _ (chdir "trajectory_msgs"))))))
+      (home-page "https://wiki.ros.org/trajectory_msgs")
+      (synopsis "Messages for defining robot trajectories")
+      (description "Messages for defining robot trajectories.
+These messages are also the building blocks of most of the control_msgs actions.")
+      (license license:bsd-3))))
+
 (define-public ros-noetic-rospack
   (let ((commit "63bf5288618e6a44e2f12fbd43ea6fd4e6b5e984")
         (revision "0"))
@@ -1881,5 +1913,4 @@ client libraries (roscpp, rospy) and graph introspection tools (rostopic, rosnod
 ;;~~  - common_msgs
 ;;~~  - ros_core
 ;;~~  - rosconsole_bridge
-;;~~  - trajectory_msgs
 ;;~~  - visualization_msgs
