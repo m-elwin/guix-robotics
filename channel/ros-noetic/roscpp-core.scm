ab125fbb111af97d6d92e9f0d836b9fb95621822
Matthew Elwin <elwin@northwestern.edu>
2025-05-23 15:58:00 -0500

    Dividing into more scheme modules.
    Trying to keep meta-packages toegether to clean up dependencies.

---
+++ channel/ros-noetic/roscpp-core.scm
@@ -0,0 +1,110 @@
+(define-module (ros-noetic roscpp-core)
+  #:use-module (guix build-system catkin)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix download)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix search-paths)
+  #:use-module (guix gexp)
+  #:use-module (guix utils)
+  #:use-module (gnu packages apr)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages boost)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages cpp)
+  #:use-module (gnu packages commencement)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages lisp)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-crypto)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages xml)
+  #:use-module (contributed))
+
+(define-public ros-noetic-cpp-common
+  (let ((commit "2951f0579a94955f5529d7f24bb1c8c7f0256451")
+        (revision "0"))
+    (package
+      (name "ros-noetic-cpp-common")
+      (version (git-version "0.7.3" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/ros/roscpp_core")
+                             (commit commit)))
+         (sha256
+          (base32 "0zs0wlkldjkvyi2d74ri93hykbq2a5wmkb1x0jibnashlyiijiwj"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (native-inputs (list boost console-bridge))
+      (arguments (list
+                  #:phases #~(modify-phases %standard-phases
+                               ;; go to the directory for the ros package
+                     (add-after 'unpack 'switch-to-pkg-src
+                       (lambda _ (chdir "cpp_common"))))))
+      (home-page "https://github.com/ros/roscpp_core")
+      (synopsis "C++ code for doing things that are not necessarily ROS related")
+      (description "C++ code for doing things that are not necessarily ROS
+related, but are useful for multiple packages. This includes things like
+the ROS_DEPRECATED and ROS_FORCE_INLINE macros, as well as code for getting
+backtraces. This package is part of roscpp")
+      (license license:bsd-3))))
+
+(define-public ros-noetic-rostime
+  (let ((commit "2951f0579a94955f5529d7f24bb1c8c7f0256451")
+        (revision "0"))
+    (package
+      (name "ros-noetic-rostime")
+      (version (git-version "0.7.3" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/ros/roscpp_core")
+                             (commit commit)))
+         (sha256
+          (base32 "0zs0wlkldjkvyi2d74ri93hykbq2a5wmkb1x0jibnashlyiijiwj"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (native-inputs (list boost ros-noetic-cpp-common))
+      (arguments (list
+                  #:phases #~(modify-phases %standard-phases
+                               ;; go to the directory for the ros package
+                     (add-after 'unpack 'switch-to-pkg-src
+                       (lambda _ (chdir "rostime"))))))
+      (home-page "http://wiki.ros.org/roscpp/Overview/Time")
+      (synopsis "Time and Duration implementation for C++ libraries, including roscpp")
+      (description "Time and Duration implementation for C++ libraries, including for roscpp.
+This package is a part of roscpp.")
+      (license license:bsd-3))))
+
+(define-public ros-noetic-roscpp-core
+  (let ((commit "2951f0579a94955f5529d7f24bb1c8c7f0256451")
+        (revision "0"))
+    (package
+      (name "ros-noetic-roscpp-core")
+      (version (git-version "0.7.3" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/ros/roscpp_core")
+                             (commit commit)))
+         (sha256
+          (base32 "0zs0wlkldjkvyi2d74ri93hykbq2a5wmkb1x0jibnashlyiijiwj"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (native-inputs (list ros-noetic-cpp-common ros-noetic-roscpp-serialization ros-noetic-roscpp-traits ros-noetic-rostime))
+      (arguments (list
+                  #:phases #~(modify-phases %standard-phases
+                               ;; go to the directory for the ros package
+                     (add-after 'unpack 'switch-to-pkg-src
+                       (lambda _ (chdir "roscpp_core"))))))
+      (home-page "http://wiki.ros.org/roscpp")
+      (synopsis "Underlying data libraries for roscpp messages")
+      (description "Underlying data libraries for roscpp messages.ros.org/wiki/roscpp/Overview/MessagesTraits.
+This package is a part of roscpp.")
+      (license license:bsd-3))))
