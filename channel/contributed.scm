cecf9485fc33bad47b63015730e005eec57c60eb
Matthew Elwin <elwin@northwestern.edu>
2025-06-03 09:19:18 -0500

    Removed log4cxx since that is now in upstream guix!

--- channel/contributed.scm
+++ channel/contributed.scm
@@ -12,33 +12,13 @@
   #:use-module (gnu packages bash)
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
+  #:use-module (gnu packages logging)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages python-build)
   #:use-module (gnu packages version-control)
   )
 ;; Items that are contributed to gnu guix and pending review
-
-(define-public log4cxx ; gnu bug 78395
-  (package
-    (name "log4cxx")
-    (version "1.4.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append "mirror://apache/logging/log4cxx/" version
-                           "/apache-log4cxx-" version ".tar.gz"))
-       (sha256
-        (base32 "15kgxmqpbg8brf7zd8mmzr7rvm9zrigz3ak34xb18v2ld8siyb9x"))))
-    (build-system cmake-build-system)
-    (native-inputs (list apr apr-util pkg-config zip))
-    (home-page "https://logging.apache.org/log4cxx/")
-    (synopsis "C++ logging library patterned after Apache log4j")
-    (description
-     "A C++ logging framework patterned after Apache
-log4j which uses Apache Portable Runtime for most platform-specific code
-and should be usable on any platform supported by APR")
-    (license license:asl2.0)))
 
 (define-public log4cxx-0.11
   (package
