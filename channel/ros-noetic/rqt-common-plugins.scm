6eb552b6c7fbc7c6f89d0525bbd0917787dbebd5
Matthew Elwin <elwin@northwestern.edu>
2025-06-13 15:39:33 -0500

    Added rqt-logger level and prototyped some more

--- channel/ros-noetic/rqt-common-plugins.scm
+++ channel/ros-noetic/rqt-common-plugins.scm
@@ -15,11 +15,16 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with Guix-Robotics.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (ros-noetic rqt)
+(define-module (ros-noetic rqt-common-plugins)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system catkin)
   #:use-module (guix packages)
-  #:use-module (guix git-download))
+  #:use-module (guix git-download)
+  #:use-module (ros-noetic bootstrap)
+  #:use-module (ros-noetic ros)
+  #:use-module (ros-noetic ros-comm)
+  #:use-module (ros-noetic ros-visualization)
+  #:use-module (ros-noetic rqt))
 
 ;;; Commentary:
 ;;;
@@ -29,6 +34,122 @@
 ;;;
 ;;; Code:
 
+(define-public ros-noetic-rqt-logger-level
+  (let ((commit "cd551135b8a33ec2133409807a12ab49ab6ba2ed")
+        (revision "0"))
+    (package
+      (name "ros-noetic-rqt-logger-level")
+      (version (git-version "0.4.11" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/ros-visualization/rqt_logger_level")
+               (commit commit)))
+         (sha256
+          (base32 "058vc6zjl03rsa3r92h8m211bgx8hznl0fa1p6vw098hvpw6phbv"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (propagated-inputs (list python-rospkg
+                               ros-noetic-python-qt-binding
+                               ros-noetic-rosnode
+                               ros-noetic-rosservice
+                               ros-noetic-rospy
+                               ros-noetic-rqt-gui
+                               ros-noetic-rqt-gui-py))
+      (home-page "https://wiki.ros.org/rqt_logger_level")
+      (synopsis "GUI plugin for configuring the logger level or ROS nodes")
+      (description "GUI plugin for configuring the logger level or ROS nodes.")
+      (license license:bsd-3))))
+
+(define-public ros-noetic-rqt-console
+  (let ((commit "6084e593dc362850f5fa20961c5dc25082d6143b")
+        (revision "0"))
+    (package
+      (name "ros-noetic-rqt-console")
+      (version (git-version "0.4.11" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/ros-visualization/rqt_console")
+               (commit commit)))
+         (sha256
+          (base32 "0iwspznn0dmjhf0lbv7snjj17gadrmmzsbvp21sjpmjfimznifl9"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (propagated-inputs (list python-rospkg
+                               ros-noetic-python-qt-binding
+                               ros-noetic-roslib
+                               ros-noetic-rospy
+                               ros-noetic-rqt-gui
+                               ros-noetic-rqt-gui-py
+                               ros-noetic-rqt-logger-level
+                               ros-noetic-rqt-py-common
+                               ros-noetic-rqt-console))
+      (home-page "https://wiki.ros.org/rqt_console")
+      (synopsis "GUI plugin for filtering and displaying ROS messages")
+      (description "GUI plugin for filtering and displaying ROS messages")
+      (license license:bsd-3))))
+
+(define-public ros-noetic-rqt-msg
+  (let ((commit "ae57b4fbaef46e053f2844dbc164ffb4697763e9")
+        (revision "0"))
+    (package
+      (name "ros-noetic-rqt-msg")
+      (version (git-version "0.4.11" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/ros-visualization/rqt_msg")
+               (commit commit)))
+         (sha256
+          (base32 "0iwspznn0dmjhf0lbv7snjj17gadrmmzsbvp21sjpmjfimznifl9"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (propagated-inputs (list python-rospkg
+                               ros-noetic-python-qt-binding
+                               ros-noetic-roslib
+                               ros-noetic-rosmsg
+                               ros-noetic-rospy
+                               ros-noetic-rqt-gui
+                               ros-noetic-rqt-gui-py
+                               ros-noetic-rqt-py-common
+                               ros-noetic-rqt-console))
+      (home-page "https://wiki.ros.org/rqt_msg")
+      (synopsis "Python GUI plugin for introspecting ROS message types")
+      (description "Python GUI plugin for introspecting ROS message types.
+Note that the msgs available through this plugin are the ones stored on your machine,
+not on the ROS core your rqt instance connects to.")
+      (license license:bsd-3))))
+
+(define-public ros-noetic-rqt-action
+  (let ((commit "0832bd7c6fa45afae0289bc514413f820be4afde")
+        (revision "0"))
+    (package
+      (name "ros-noetic-rqt-action")
+      (version (git-version "0.4.11" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/ros-visualization/rqt_action")
+               (commit commit)))
+         (sha256
+          (base32 "0iwspznn0dmjhf0lbv7snjj17gadrmmzsbvp21sjpmjfimznifl9"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (propagated-inputs (list ros-noetic-rospy
+                               ros-noetic-rqt-msg
+                               ros-noetic-rqt-py-common))
+      (home-page "https://wiki.ros.org/rqt_action")
+      (synopsis "Introspect all avalible ROS action (from actionlib types).")
+      (description "Introspect all avalible ROS action (from actionlib types).
+By using rqt_msg, the output format is unifited with it and rqt_srv.
+Note that the actions shown by this plugin are the ones that are stored
+on your machine, not on the ROS core your rqt instance connects to.")
+      (license license:bsd-3))))
 
 (define-public ros-noetic-rqt-common-plugins
   (let ((commit "73b0ebc69a6a36fbbce68d56c05dcf961a50ea59")
