f0d4b0709a8328f77e4ad6276bcde8113e4a595f
Matthew Elwin <elwin@northwestern.edu>
2025-06-30 10:36:09 -0500

    Working more on rqt

---
+++ channel/ros-noetic/rqt-robot-plugins.scm
@@ -0,0 +1,116 @@
+;;; Guix-Robotics --- GNU Guix Channel
+;;; Copyright © 2025 Matthew Elwin <elwin@northwestern.edu>
+;;; This file is part of Guix-Robotics.
+;;;
+;;; Guix-Robotics is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; Guix-Robotics is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Guix-Robotics.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (ros-noetic rqt-robot-plugins)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix build-system catkin)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (ros-noetic bootstrap)
+  #:use-module (ros-noetic common-msgs)
+  #:use-module (ros-noetic qt-gui-core)
+  #:use-module (ros-noetic ros)
+  #:use-module (ros-noetic robot)
+  #:use-module (ros-noetic ros-base)
+  #:use-module (ros-noetic ros-comm)
+  #:use-module (ros-noetic ros-core)
+  #:use-module (ros-noetic ros-visualization)
+  #:use-module (ros-noetic image-common)
+  #:use-module (ros-noetic vision-opencv)
+  #:use-module (ros-noetic rqt))
+
+;;; Commentary:
+;;;
+;;;
+;;; Packages related to rqt_robot_plugins, graphical rqt plugins that
+;;; are useful for using with robots at runtime
+;;;
+;;; Code:
+
+(define-public ros-noetic-rqt-moveit
+  (let ((commit "001811e03927d6c51dd363c733a0d25c9a608127")
+        (revision "0"))
+    (package
+      (name "ros-noetic-rqt-moveit")
+      (version (git-version "0.5.13" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/ros-visualization/rqt_moveit")
+               (commit commit)))
+         (sha256
+          (base32 "058vc6zjl03rsa3r92h8m211bgx8hznl0fa1p6vw098hvpw6phbv"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (propagated-inputs (list ros-noetic-python-qt-binding
+                               ros-noetic-rosnode
+                               ros-noetic-rospy
+                               ros-noetic-rqt-gui
+                               ros-noetic-rqt-gui-py
+                               ros-noetic-rqt-py-common
+                               ros-noetic-rqt-topic
+                               ros-noetic-sensor-msgs))
+      (home-page "https://wiki.ros.org/rqt_moveit")
+      (synopsis "Rqt tool for monitoring MoveIt! planner tasks")
+      (description "Assists in monitoring tasks for the MoveIt! motion planner
+framework. Currently the following items are monitored if they are either
+running, existing, or published:
+Node: /movegroup
+Parameter: /robot_description /robot_description_semantic
+Topic: sensor_msgs/PointCloud, sensor_msgs/PointCloud2
+       sensor_msgs/Image, sensor_msgs/CameraInfo
+
+This package is not made by the MoveIt! development team.")
+      (license license:bsd-3))))
+
+(define-public ros-noetic-rqt-robot-plugins
+  (let ((commit "aa078795cddead2dc6232acc519364d65696873f")
+        (revision "0"))
+    (package
+      (name "ros-noetic-rqt-robot-plugins")
+      (version (git-version "0.5.8" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/ros-visualization/rqt_robot_plugins")
+               (commit commit)))
+         (sha256
+          (base32 "0bi82bbvpqdasnabxxahiks3rw1hblpchn5lrjjnk40i41f9r7jk"))
+         (file-name (git-file-name name version))))
+      (build-system catkin-build-system)
+      (propagated-inputs (list
+                          ros-noetic-rqt-moveit
+                          ros-noetic-rqt-nav-view
+                          ros-noetic-rqt-pose-view
+                          ros-noetic-rqt-robot-dashboard
+                          ros-noetic-rqt-robot-monitor
+                          ros-noetic-rqt-robot-steering
+                          ros-noetic-rqt-runtime-monitor
+                          ros-noetic-rqt-rviz
+                          ros-noetic-rqt-tf-tree))
+      (home-page "https://wiki.ros.org/rqt_robot_plugins")
+      (synopsis "Visual rqt plugins particularly useful at robot runtime")
+      (description
+       "Visual rqt plugins particularly useful at robot runtime.
+TO run any rqt plugins, just type in 'rqt' then select the plugins from the gui.")
+      (license license:bsd-3))))
